5a46a420fed4ea1cf443351f22ae3438f38adc28
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-08 15:23:42 -0500

    commit tree df1d7e5b97a10e1a0d9d0b45ca5b87b775adc5e6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 !=0 && @side1 == @side2 && @side1 == @side3 then :equilateral
+    elsif (@side1 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side1 != @side2) || (@side2 == @side3 && @side1 != @side2) then :isosceles
+    elsif @side1 != @side2 && @side1 != @side3 then :scalene
+    elsif @side1 == 0 || @side2 == 0 || @side3 == 0
+      # begin 
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts "Triangle::TriangleError"
+      # end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+
+puts Triangle.new(0, 0, 0).kind
